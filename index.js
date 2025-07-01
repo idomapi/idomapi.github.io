99190fdaf0036d0c605b03fa2864f359e88eba9c
Ido Zamir <idoz@mapi.gov.il>
2025-07-01 17:04:46 +0300

    api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function initGovMap() {
     govmap.createMap('map', {
-        token: '8afbb7f6-f247-4b73-9366-635aaa7c9b1f',
+        token: 'ce39f4d4-93ac-4f6f-bb70-9618a4c6b657',
         layers: ["GASSTATIONS", "SUB_GUSH_ALL", "211923"],
         visibleLayers: ["211923"],
         showXY: true,
@@ -42,15 +42,15 @@ function selectFeaturesOnMapParcel() {
         drawType: govmap.drawType.Polygon,
         filterLayer: false,
         isZoomToExtent: true,
-        layers: ['SUB_GUSH_ALL', 'layer_211923'],
+        layers: ['SUB_GUSH_ALL', '201923'],
         returnFields: {
             SUB_GUSH_ALL: ['objectid', 'gush_num', 'status_text'],
-            layer_211923: ['value0', 'value1']
+            201923: ['value0', 'value1']
         },
         selectOnMap: true,
         whereClause: {
             SUB_GUSH_ALL: "(gush_num IN(7103, 7101))",
-            layer_211923: "(value1 >= 18)"
+            201923: "(value0 >= 4)"
         },
     }
     govmap.selectFeaturesOnMap(params).then(function (response) {

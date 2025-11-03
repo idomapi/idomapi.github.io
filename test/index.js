c5721ba231e5ff0067fff6b5ee72e9657219f49f
Ido Zamir <idoz@mapi.gov.il>
2025-11-03 12:00:38 +0200

    m

--- test/index.js
+++ test/index.js
@@ -5,8 +5,8 @@ function initGovMap() {
             populateDropdown();
         },
         token: 'ce39f4d4-93ac-4f6f-bb70-9618a4c6b657',
-        layers: ["GASSTATIONS", "SUB_GUSH_ALL", "PARCEL_ALL", "layer_202845"],
-        visibleLayers: ['layer_202845'],
+        layers: ["GASSTATIONS", "SUB_GUSH_ALL", "PARCEL_ALL", "202845"],
+        visibleLayers: ['202845'],
         showXY: true,
         isEmbeddedToggle: false,
         background: "0",

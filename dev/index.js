ed9b160223a62cd376eaeb6fe72a0631be615d53
Ido Zamir <idoz@mapi.gov.il>
2025-12-14 08:56:54 +0200

    ,

--- dev/index.js
+++ dev/index.js
@@ -1,7 +1,7 @@
 function initGovMap() {
     govmap.createMap('map', {
         token: '8c430f7f-1e21-4434-b256-c5e91fac4005',
-        layers: ["GASSTATIONS", "SUB_GUSH_ALL", 'bus_stops', '154006'],
+        layers: ["GASSTATIONS", "SUB_GUSH_ALL", 'bus_stops'],
         visibleLayers: ['PARCEL_ALL'],
         showXY: true,
         isEmbeddedToggle: false,

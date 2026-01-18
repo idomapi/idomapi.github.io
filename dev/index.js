292f66fdd6fb07f5595521726b3dddcccb701d1c
Ido Zamir <idoz@mapi.gov.il>
2026-01-18 15:34:45 +0200

    dev

--- dev/index.js
+++ dev/index.js
@@ -1,8 +1,5 @@
-const map1 = 'map';
-const map2 = 'map2';
-
 function initGovMap() {
-    govmap.createMap(map1, {
+    govmap.createMap('map', {
         token: '8c430f7f-1e21-4434-b256-c5e91fac4005',
         layers: ["layer_153830", 'ra_gvulot_rm'],
         showXY: true,
@@ -12,15 +9,6 @@ function initGovMap() {
         center: { x: 179487, y: 663941 },
         level: 9,
         onLoad: function () {
-            listener(map1);
         }
     });
-}
-
-function listener(map) {
-    govmap.onEvent(govmap.events.CLICK).progress(e => console.log(map, 'listener - CLICK', e));
-}
-
-function unbindEvent(map) {
-    govmap.unbindEvent(govmap.events.CLICK);
 }

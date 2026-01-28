6b44436966b64d077947fdb39eb6b8068cdd148b
Ido Zamir <idoz@mapi.gov.il>
2026-01-28 09:36:22 +0200

    143

--- index.js
+++ index.js
@@ -9,8 +9,22 @@ function initGovMap() {
         bgButton: true,
         background: "0",
         layersMode: 1,
-        center: { x: 179487, y: 663941 },
+        center: { x: 163868, y: 578293 }, // { x: 179487, y: 663941 },
         level: 4,
         onLoad: function () {},
     });
+}
+
+function searchInLayer(values) {
+    var params = {
+        layerName: '143',
+        fieldName: 'lotidint',
+        fieldValues: values,
+        highlight: true,
+        showBubble: false,
+        outLineColor: [100, 0, 0, 50],
+        fillColor: [200, 0, 0, 150]
+    };
+
+    govmap.searchInLayer(params);
 }

46d0173d28163c5cabaf8b87fdde90ecf495408e
Ido Zamir <idoz@mapi.gov.il>
2026-02-11 14:46:17 +0200

    .

--- saroussi/index.js
+++ saroussi/index.js
@@ -45,52 +45,6 @@ function displayGeometriesPolygon() {
     });
 }
 
-function displayGeometriesPoint() {
-    var data = {
-        wkts: ["POINT(151612.40 534674.88)"],
-        names: ['p2'],
-        geometryType: govmap.geometryType.POINT,
-        defaultSymbol:
-        {
-            url: 'https://avatars.githubusercontent.com/u/196661916?v=4&size=64',
-            width: 45,
-            height: 45
-        },
-        symbols: [
-            { url: 'https://avatars.githubusercontent.com/u/196661916?v=4&size=64', width: 45, height: 45 },
-        ],
-        clearExisting: false,
-        data: {
-            tooltips: ['נקודה 2'],
-            headers: ['נקודה'],
-            bubbles: ['נקודה'],
-            bubbleUrl: 'https://he.wikipedia.org/wiki/%D7%A4%D7%9C%D7%90%D7%A4%D7%9C'
-        }
-    };
-    govmap.displayGeometries(data).then(function (response) {
-        console.log(response.data);
-    });
-}
-function displayGeometriesCircle() {
-    var data = {
-        circleGeometries: [{ x: 151612.40, y: 534674.88, radius: 1500 }],
-        names: ['p3'],
-        geometryType: govmap.geometryType.CIRCLE,
-        defaultSymbol:
-        {
-            outlineColor: [0, 0, 255, 1],
-            outlineWidth: 1,
-            fillColor: [0, 255, 0, 0.5]
-        },
-        symbols: [],
-        clearExisting: false,
-        data: {
-            headers: ['מעגל'],
-            bubbles: ['מעגל'],
-            bubbleUrl: 'https://he.wikipedia.org/wiki/%D7%A1%D7%91%D7%99%D7%97'
-        }
-    };
-    govmap.displayGeometries(data).then(function (response) {
-        console.log(response.data);
-    });
+function draw() {
+    govmap.draw(govmap.drawType.Point);
 }

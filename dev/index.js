8839f9e6969de3e18850421f718a1d7077130fe5
Ido Zamir <idoz@mapi.gov.il>
2026-02-12 10:17:44 +0200

    .

--- dev/index.js
+++ dev/index.js
@@ -1,7 +1,7 @@
 async function initGovMap() {
     govmap.createMap('map', {
         token: '8c430f7f-1e21-4434-b256-c5e91fac4005',
-        layers: ["layer_153830", 'layer_154112'],
+        layers: ["layer_153830", 'layer_154112', 'parcel_all'],
         visibleLayers: ["layer_153830", 'layer_154112'],
         showXY: true,
         isEmbeddedToggle: false,

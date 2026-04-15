4b704a671a463233c20c234019a7f6d7a2029deb
Ido Zamir <idoz@mapi.gov.il>
2026-04-15 11:27:14 +0300

    .

--- sanity/const.js
+++ sanity/const.js
@@ -1,4 +1,4 @@
-const env = 'stage';
+const env = 'prod';
 // const GOVMAP_TOKEN = '8c430f7f-1e21-4434-b256-c5e91fac4005'; // dev
 const GOVMAP_TOKEN = 'ce39f4d4-93ac-4f6f-bb70-9618a4c6b657'; // stage
 // const GOVMAP_TOKEN = '8afbb7f6-f247-4b73-9366-635aaa7c9b1f'; // production

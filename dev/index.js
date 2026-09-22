cc9c5f0aca2712889f7ee75c9af36d54aa228838
Ido Zamir <idoz@mapi.gov.il>
2026-09-22 16:45:21 +0300

    .

--- dev/index.js
+++ dev/index.js
@@ -4,8 +4,8 @@ const migrashimLayer = '162206';
 async function initGovMap() {
     govmap.createMap('map', {
         token: '8c430f7f-1e21-4434-b256-c5e91fac4005',
-        layers: ["statistic_areas_2011"],
-        visibleLayers: [migrashimLayer, gvulLayer],
+        layers: ['163451'],
+        visibleLayers: ['layer_211257'],
         showXY: true,
         isEmbeddedToggle: false,
         background: 0,
@@ -102,8 +102,40 @@ Driver's name : GPS
     });
 }
 
+// function filterLayer() {
+//     const ma = 2056235;
+//     govmap.filterLayers({ 'layerName': migrashimLayer, 'whereClause': "MISHASAVA =" + ma, 'zoomToExtent': false });
+//     govmap.filterLayers({ 'layerName': gvulLayer, 'whereClause': "MISHASAVA =" + ma, 'zoomToExtent': true });
+// }
+
 function filterLayer() {
-    const ma = 2056235;
-    govmap.filterLayers({ 'layerName': migrashimLayer, 'whereClause': "MISHASAVA =" + ma, 'zoomToExtent': false });
-    govmap.filterLayers({ 'layerName': gvulLayer, 'whereClause': "MISHASAVA =" + ma, 'zoomToExtent': true });
+    // var params = {
+    //     layerName: '163451',
+    //     whereClause: "(serviceid IN (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,209,210) AND (airisktype LIKE '%ניתוק חברתי%' OR airisktype LIKE '%ירידה חושית%'))",
+    //     zoomToExtent: true,
+    // };
+    // govmap.filterLayers(params);
+    const params = {
+        "geometry": "POINT (179322 665224)",
+        "layerName": "163451",
+        "fields": [
+            "servicenam",
+            "fulladdres",
+            "servicedes",
+            "targetpopu",
+            "requirespa",
+            "requires_1",
+            "servicepro",
+            "language",
+            "airisktype",
+            "accessibil",
+            "providerna",
+            "gisx",
+            "gisy"
+        ],
+        "radius": 150,
+        "whereClause": "(serviceid IN (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107,108,109,110,111,112,113,114,115,116,117,118,119,120,121,122,123,124,125,126,127,128,129,130,131,132,133,134,135,136,137,138,139,140,141,142,143,144,145,146,147,148,149,150,151,152,153,154,155,156,157,158,159,160,161,162,163,164,165,166,167,168,169,170,171,172,173,174,175,176,177,178,179,180,181,182,183,184,185,186,187,188,189,190,191,192,193,194,195,196,197,198,199,200,201,202,203,204,205,206,207,208,209,210, 237) AND airisktype LIKE '%ירידה חושית%')"
+    };
+    govmap.intersectFeatures(params)
+        .then((response) => {console.log('intersectFeatures response', response);});
 }
